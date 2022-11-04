bd5e9d8144b6d4b8839e0ad1b9eefee6f116b867
bwyanyuuu <55642633+bwyanyuuu@users.noreply.github.com>
2022-11-04 13:42:10 +0800

    cccc-221104-1342

--- Assets/Chapters/Ch2 RabbitHole/scripts/PockerController.cs
+++ Assets/Chapters/Ch2 RabbitHole/scripts/PockerController.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class PockerController : MonoBehaviour
 {
     public GameObject cards;
-    //private List<Vector3> trans = new List<Vector3>();
     public Vector3[] pos;
     public Quaternion[] rot;
-    //private GameObject card;
     private RotateRoom rotateRoom;
-    public GameObject emit;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +26,6 @@ public class PockerController : MonoBehaviour
     {
         //print("sss");
     }
-    public void hoverColor()
-    {
-        //card.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
-    }
     public void moveOut(GameObject card)
     {
         int idx = int.Parse(card.name);
@@ -43,16 +36,17 @@ public class PockerController : MonoBehaviour
         //card2.transform.rotation = trans[idx].rotation;
         //card2.SetActive(true);
         card.transform.parent = gameObject.transform;
-        rotateRoom.rotate(idx);
+        card.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().enabled = true;
+        card.GetComponent<UnityEngine.Playables.PlayableDirector>().Play();
+        //card.GetComponent<UnityEngine.Playables.PlayableDirector>().enabled = true;
+
 
         //emit.GetComponent<CurlNoiseParticleSystem.Emitter.ShapeEmitter>().Emit();
-        //StartCoroutine(disappear(card));
+        StartCoroutine(disappear(idx));
     }
-    IEnumerator disappear(GameObject card)
+    IEnumerator disappear(int idx)
     {
-        yield return new WaitForSeconds(1);
-        card.SetActive(false);
-        yield return new WaitForSeconds(2);
-        card.GetComponent<CurlNoiseParticleSystem.Emitter.ShapeEmitter>().Emit();
+        yield return new WaitForSeconds(1.5f);
+        rotateRoom.rotate(idx);
     }
 }
