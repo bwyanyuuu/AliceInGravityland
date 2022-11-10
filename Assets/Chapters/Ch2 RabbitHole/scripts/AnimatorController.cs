90ffd0508f757f341b42501d816f407bb5e77963
s8617527 <43349795+s8617527@users.noreply.github.com>
2022-11-10 18:11:18 +0800

    Book-221110-1810

--- Assets/Chapters/Ch2 RabbitHole/scripts/AnimatorController.cs
+++ Assets/Chapters/Ch2 RabbitHole/scripts/AnimatorController.cs
@@ -6,20 +6,33 @@ public class AnimatorController : MonoBehaviour
 {
     
     public Animator anim;
+    float i = 0.0f;
 
+    void Start()
+    {
+        anim.speed = 0f;
+    }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.H))
         {
             // AnimatorManager.Instance.PlayAnim(anim, 1f);//一倍速播放
-            anim.SetFloat("speed", 1.0f);
+            // anim.SetFloat("speed", 1.0f);
+            perframe(anim, i);
+            i = i + 0.02f;
+            if(i > 1.0f) i = 1.0f;
+            Debug.Log(i);
         }
         
         else if(Input.GetKeyDown(KeyCode.F))
         {
             // AnimatorManager.Instance.PlayAnim(anim, -1f);//一倍速倒放
-            anim.SetFloat("speed", -1.0f);
+            // anim.SetFloat("speed", -1.0f);
+            perframe(anim, i);
+            i = i - 0.02f;
+            if(i < 0.0f) i = 0.0f;
+            Debug.Log(i);
         }
         
         else if(Input.GetKeyDown(KeyCode.G))
@@ -27,5 +40,9 @@ public class AnimatorController : MonoBehaviour
             anim.SetFloat("speed", 0.0f);
         }
 
+    }
+    public void perframe(Animator anim, float idx){
+        anim.Play("Scene",0,idx);
+        anim.SetFloat("speed", 0.0f);
     }
 }
