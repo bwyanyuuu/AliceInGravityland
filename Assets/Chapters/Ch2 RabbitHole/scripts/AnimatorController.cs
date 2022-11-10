bf6dc2368115ca13bb06c92586930982edac6c27
s8617527 <43349795+s8617527@users.noreply.github.com>
2022-11-11 02:06:04 +0800

    Book-221111-0205

--- Assets/Chapters/Ch2 RabbitHole/scripts/AnimatorController.cs
+++ Assets/Chapters/Ch2 RabbitHole/scripts/AnimatorController.cs
@@ -6,36 +6,62 @@ public class AnimatorController : MonoBehaviour
 {
     
     public Animator anim;
+    public GameObject knob;//旋轉鈕
     float i = 0.0f;
+    float KnobAngle = 0.0f;
 
     void Start()
     {
         anim.speed = 0f;
+        KnobAngle = knob.transform.eulerAngles.y;
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.H))
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log(knob.transform.eulerAngles.y);
+        }
+        if (knob.transform.eulerAngles.y - KnobAngle > 0)
+        {
+            perframe(anim, i);
+            i = i + 0.002f* (knob.transform.eulerAngles.y - KnobAngle)*0.001f;
+            if(i > 1.0f) i = 1.0f;
+            Debug.Log(i);
+        }
+        if (knob.transform.eulerAngles.y - KnobAngle < 0)
+        {
+            perframe(anim, i);
+            i = i - 0.002f * (knob.transform.eulerAngles.y - KnobAngle)*0.001f;
+            if(i < 0.0f) i = 0.0f;
+            Debug.Log(i);
+        }
+        if (knob.transform.eulerAngles.y - KnobAngle == 0)
+        {
+            anim.SetFloat("speed", 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
         {
             // AnimatorManager.Instance.PlayAnim(anim, 1f);//一倍速播放
             // anim.SetFloat("speed", 1.0f);
             perframe(anim, i);
-            i = i + 0.02f;
+            i = i + 0.002f;
             if(i > 1.0f) i = 1.0f;
             Debug.Log(i);
         }
         
-        else if(Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             // AnimatorManager.Instance.PlayAnim(anim, -1f);//一倍速倒放
             // anim.SetFloat("speed", -1.0f);
             perframe(anim, i);
-            i = i - 0.02f;
+            i = i - 0.002f;
             if(i < 0.0f) i = 0.0f;
             Debug.Log(i);
         }
         
-        else if(Input.GetKeyDown(KeyCode.G))
+        if(Input.GetKeyDown(KeyCode.G))
         {
             anim.SetFloat("speed", 0.0f);
         }
