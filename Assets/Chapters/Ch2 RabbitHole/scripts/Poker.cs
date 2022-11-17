4f229f7bce4813ead39e2429b989d76bab8061a4
s8617527 <43349795+s8617527@users.noreply.github.com>
2022-11-17 14:59:04 +0800

    book-221117-1459

--- Assets/Chapters/Ch2 RabbitHole/scripts/Poker.cs
+++ Assets/Chapters/Ch2 RabbitHole/scripts/Poker.cs
@@ -6,18 +6,34 @@ public class Poker : MonoBehaviour
 {
     private AnimatorClipInfo clip;
     private Animator animator;
+    private AnimatorStateInfo stateinfo;
     // Start is called before the first frame update
     void Start()
     {
-        clip = gameObject.GetComponent<AnimatorClipInfo>();
+        // clip = gameObject.GetComponent<AnimatorClipInfo>();
         animator = gameObject.GetComponent<Animator>();
+        // animator.speed = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(clip.clip.name == "blank")
+        // if(clip.clip.name == "blank")
+        // {
+        //     animator.enabled = false;
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.B))
+        // {
+        //     animator.speed = 1.0f;
+        //     animator.Play("poker_4",0,0.0f);
+        //     Debug.Log("start");
+        // }
+
+        stateinfo = animator.GetCurrentAnimatorStateInfo(0); 
+        if ((stateinfo.normalizedTime >= 1.0f) && (stateinfo.IsTag("Poker")))
         {
+            // Debug.Log("end");
             animator.enabled = false;
         }
     }
