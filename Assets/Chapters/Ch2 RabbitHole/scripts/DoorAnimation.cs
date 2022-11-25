abfe553e3e47e26985a7024f44cc66219343eaae
s8617527 <43349795+s8617527@users.noreply.github.com>
2022-11-26 02:01:09 +0800

    book-221126-0200

--- Assets/Chapters/Ch2 RabbitHole/scripts/DoorAnimation.cs
+++ Assets/Chapters/Ch2 RabbitHole/scripts/DoorAnimation.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class DoorAnimation : MonoBehaviour
 {
     public GameObject player;
-    // public GameObject AnimPlayer;
+    public GameObject timeline;
+    public GameObject wall;
     // Start is called before the first frame update
     void Start()
     {
         player.transform.parent = null;
         player.transform.SetParent(this.transform, false);
-        player.transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
+        player.transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);    
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeline.GetComponent<PlayableDirector>().state == PlayState.Paused)
+        {
+            player.transform.SetParent(GameObject.Find("House").transform, true);
+            wall.GetComponent<BoxCollider>().enabled = true;
+        }
     }
 }
